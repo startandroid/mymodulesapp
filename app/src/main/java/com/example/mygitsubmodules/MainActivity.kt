c616c652b7ad8f483a059c98e5f92b793e5d5b4a
Dmitry Pliskov <d.pliskov@godeltech.com>
2024-06-28 13:41:32 +0300

    fix spaces

--- app/src/main/java/com/example/mygitsubmodules/MainActivity.kt
+++ app/src/main/java/com/example/mygitsubmodules/MainActivity.kt
@@ -13,8 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mygitsubmodules.ui.theme.MyGitSubmodulesTheme
 
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
